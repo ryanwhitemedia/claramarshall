d9f41fbee4a0edf4c8faa721ee784f21afe71fcc
Ryan White <hello@ryanwhitemedia>
2020-12-13 22:32:08 -0500

    header styles

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,15 +1,39 @@
-import React from "react"
-
+import React, { useState } from "react"
+import Link from "gatsby-link"
+import classnames from "classnames"
 import "./header.scss"
 
 import Logo from "../../svgs/logo.svg"
 
-const Header = () => (
-  <header className="Header">
-    <div>
-      <Logo className="logo" />
-    </div>
-  </header>
-)
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false)
+  return (
+    <header className={classnames(showMenu && "fixHeader", "Header")}>
+      <div className="innerContent">
+        <Logo className="logo" />
+        <button className="hamburger" onClick={() => setShowMenu(!showMenu)}>
+          <span className="line" />
+          <span className="line" />
+        </button>
+      </div>
+      <div className={classnames("menu", showMenu && "showMenu")}>
+        <nav>
+          <ul className="list">
+            <Link className="listItem" to="/">
+              Home
+            </Link>
+            <Link className="listItem" to="/">
+              Projects
+            </Link>
+            <Link className="listItem" to="/">
+              Contact
+            </Link>
+            <span role="presentation" className="circle" />
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
 
 export default Header
