72d4a3582f5f190feb13e59a4e038e83a1872dd8
Ryan White <hello@ryanwhitemedia>
2020-12-19 10:57:30 -0500

    add projects page hover styles

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,39 +1,124 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 import { graphql } from "gatsby"
-
+import classnames from "classnames"
 import Link from "gatsby-link"
+import Img from "gatsby-image"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Arrow from "../svgs/arrow.svg"
 
 import "../styles/projects.scss"
 
 export default ({ data }) => {
   const circleRef = useRef(null)
+  const [itemHovered, setItemHovered] = useState(false)
+  const [activeProject, setActiveProject] = useState(null)
+  const [circlePosition, setCirclePosition] = useState(null)
+  const [listActive, setListActive] = useState(true)
   const groups = data.allWpProject.group
   groups.sort(function (a, b) {
     return b.fieldValue - a.fieldValue
   })
+
+  const validatePosition = position => {
+    if (position <= 0) {
+      return 0
+    } else {
+      return position - 132
+    }
+  }
+
+  const hoverProject = (e, project) => {
+    const position = validatePosition(
+      e.currentTarget.getBoundingClientRect().top
+    )
+    setCirclePosition(position)
+    setActiveProject(project)
+    setItemHovered(true)
+    setListActive(false)
+  }
+
+  const hoverOffProject = () => {
+    setCirclePosition(null)
+    setItemHovered(false)
+
+    setTimeout(() => {
+      setListActive(true)
+      setActiveProject(null)
+    }, 750)
+  }
+
   return (
     <Layout>
       <SEO title="Projects" />
       <div className="Projects">
-        <span className="circle" ref={circleRef} />
-        {groups.map(group => {
-          return (
-            <div key={group.fieldValue} className="projectGroup">
-              <h3 className="groupTitle">{group.fieldValue}</h3>
-              <ul className="list">
-                {group.edges.map(project => (
-                  <li key={project.node.title} className="listItem">
-                    <Link className="link" to={`/project/${project.node.slug}`}>
-                      {project.node.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )
-        })}
+        <div
+          className={classnames("circle", itemHovered && "circleActive")}
+          style={{
+            top:
+              itemHovered && circlePosition !== null
+                ? `${circlePosition}px`
+                : null,
+          }}
+          ref={circleRef}
+          onMouseLeave={() => {
+            hoverOffProject()
+          }}
+        >
+          {activeProject !== null && itemHovered && (
+            <Link to={`/project/${activeProject.node.slug}`} className="link">
+              <span>
+                <h2 className="title">{activeProject.node.title}</h2>
+                <span className="arrowContainer">
+                  <p>GO</p>
+                  <Arrow className="arrow" />
+                </span>
+              </span>
+            </Link>
+          )}
+        </div>
+        <div className="projectsInner">
+          {groups.map(group => {
+            return (
+              <div key={group.fieldValue} className="projectGroup">
+                <h3 className="groupTitle">{group.fieldValue}</h3>
+                <ul
+                  className={classnames("list", !listActive && "listInactive")}
+                >
+                  {group.edges.map(project => (
+                    <li key={project.node.title} className="listItem">
+                      <Link
+                        className="link"
+                        to={`/project/${project.node.slug}`}
+                        onMouseEnter={e => hoverProject(e, project)}
+                      >
+                        {project.node.title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )
+          })}
+          <div
+            className={classnames("overlay", itemHovered && "overlayActive")}
+          >
+            {activeProject !== null && (
+              <>
+                {activeProject.node.featuredImage !== null && (
+                  <Img
+                    className="bgImage"
+                    fluid={
+                      activeProject.node.featuredImage.node.localFile
+                        .childImageSharp.fluid
+                    }
+                  />
+                )}
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </Layout>
   )
@@ -50,6 +135,18 @@ export const query = graphql`
             slug
             project {
               projectYear
+            }
+            featuredImage {
+              node {
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData
+                    fluid {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+              }
             }
           }
         }
