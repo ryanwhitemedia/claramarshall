7997bbb4490c44ebdb17c840e4afa7e6c28d153d
Ryan White <hello@ryanwhitemedia>
2020-12-19 18:31:55 -0500

    add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,13 +33,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `clara-marshall`,
+        short_name: `claramarshall`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FBF4EF`,
+        theme_color: `#FBF4EF`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
