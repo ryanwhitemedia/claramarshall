ca2342bcc9c8d8ff2f24658b4ef2091c0c486637
Ryan White <hello@ryanwhitemedia>
2020-12-24 13:10:24 -0500

    Add new arrow/add link on contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,16 +10,34 @@ import "../styles/contact.scss"
 
 export default ({ data, path }) => {
   const content = data.allWpPage.edges[0].node.contact
+  const coded = "A8cdc@A8cdcDcdFIc88.AvD"
+  const key = "7WOCErTumL0bFwha5XlvSGq1PHYidMVD4BjzNIcJU8Qk9y62geKs3RpfZxotnA"
+  const shift = coded.length
+  let link = ""
+  for (let i = 0; i < coded.length; i++) {
+    if (key.indexOf(coded.charAt(i)) === -1) {
+      let ltr = coded.charAt(i)
+      link += ltr
+    } else {
+      let ltr = (key.indexOf(coded.charAt(i)) - shift + key.length) % key.length
+      link += key.charAt(ltr)
+    }
+  }
   return (
     <Layout path={path}>
       <SEO title="Contact" />
       <div className="Contact">
         <div>
           <span className="titleWrapper">
-            <h1
-              className="title"
-              dangerouslySetInnerHTML={{ __html: content.title }}
-            />
+            <h1 className="title">
+              <a
+                className="email"
+                target="_blank"
+                rel="noreferrer"
+                href={`mailto:${link}`}
+                dangerouslySetInnerHTML={{ __html: content.title }}
+              />
+            </h1>
           </span>
           <ul className="list">
             {content.links.map(link => (
